b14b749636dc0042a12bab0b9b5c0991accdbb48
Matt Baker <matthew.reiss.baker@gmail.com>
2012-06-20 21:35:41 -0700

    added run animation

--- vendor/run.js
+++ vendor/run.js
@@ -5,30 +5,52 @@
 
   var runHandlers = function() {
     var index, length;
-    output.innerHTML = '';
 
-    try {
-      for(index = 0, length = handlers.length; index < length; index++) {
-        handlers[index]();
+    animateDisplay(function() {
+      try {
+        for(index = 0, length = handlers.length; index < length; index++) {
+          handlers[index]();
+        }
+        output.innerHTML += '<code class=success>Program finished successfully!</code>';
+      } catch(e) {
+        output.innerHTML += '<code class=error>' + e + '</code>';
       }
-      output.innerHTML += '<code class=success>Program finished successfully!</code>';
-    } catch(e) {
-      output.innerHTML += '<code class=error>' + e + '</code>';
-    }
+    });
+  };
+
+  var animateDisplay = function(cbk) {
+    var dots = 0;
+    var placeDot = function() {
+      output.innerHTML += '<code>.</code>';
+      dots++;
+      setTimeout(function() {
+        if(dots < 3) {
+          placeDot();
+        } else {
+          clearDisplay();
+          cbk();
+        }
+      }, 200);
+    };
+    
+    clearDisplay();
+    placeDot();
+  };
+
+  var clearDisplay = function() {
+    output.innerHTML = '';
+  };
+
+  window.println = function(text) {
+    output.innerHTML += '<code class=println>' + text + '</code>';
+  };
+
+  window.frontendClass = function(handler) {
+    handlers.push(handler);
   };
 
   runButton.addEventListener('click', runHandlers);
   window.addEventListener('keypress', function(e) {
     if(e.which === 13) runHandlers();
   });
-
-  window.frontendClass = function(handler) {
-    handlers.push(handler);
-  };
-
-  window.println = function(text) {
-    output.innerHTML += '<code>' + text + '</code>';
-  };
-
-
 }(window);
