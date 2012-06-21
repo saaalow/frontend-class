92f0875dfd776364c6c16f80720c3e48d8b3cc06
Sara Low <sara.michelle.low@gmail.com>
2012-06-20 20:58:39 -0700

    started to learn about javascript. learned about variables.

---
+++ index.js
@@ -0,0 +1,24 @@
+frontendClass(function(){
+	var meep;
+	
+	meep = 5;
+	
+	println(meep);
+	
+	var matt;
+	matt = 'matt';
+	println(matt);
+	
+	var sara;
+	sara = 'sara';
+	println(sara);
+	
+	println('matt is saras boyfriend');
+	
+	var moop;
+	moop = 6;
+	println(meep + moop);
+	
+	var wow = 1000000;
+	println(wow);
+});;
