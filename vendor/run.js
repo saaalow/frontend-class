14fa61c058b603aa4695d02e5da00f7242188d1a
Matt Baker <matthew.reiss.baker@gmail.com>
2012-06-22 02:29:26 -0700

    JS cleaning

--- vendor/run.js
+++ vendor/run.js
@@ -1,34 +1,34 @@
 !function(window) {
   var output = J('#output'),
       originalHeight = output.height(),
-      runButton = document.getElementById('run'),
-      handlers = [];
+      runButton = J('#run'),
+      stack = [],
+      running = false;
   output.updateStyle('height');
 
-  var runHandlers = function() {
+  var run = function() {
     var index, length;
 
+    running = true;
     animateDisplay(function() {
       try {
-        for(index = 0, length = handlers.length; index < length; index++) {
-          handlers[index]();
+        for(index = 0, length = stack.length; index < length; index++) {
+          stack[index]();
         }
         output.append('<code class=success>Program finished successfully!</code>');
       } catch(e) {
         output.append('<code class=error>' + e + '</code>');
       }
+      running = false;
     });
   };
 
-  var animateDisplay = function(cbk) {
-    var height,
-        dots = 0;
-    var placeDot = function() {
+  var animateDisplay = (function() {
+    var placeDots = function(dots, cbk) {
       output.append('<code>.</code>');
-      dots++;
       setTimeout(function() {
-        if(dots < 3) {
-          placeDot();
+        if(dots - 1 > 0) {
+          placeDots(dots - 1, cbk);
         } else {
           resetDisplay();
           cbk();
@@ -37,17 +37,19 @@
       }, 200);
     };
     
-    resetDisplay(placeDot);
-  };
+    return function(cbk) {
+      resetDisplay(function() { placeDots(3, cbk) });
+    };
+  })();
 
-  var resetDisplay = function(handler) {
+  var resetDisplay = function(cbk) {
     output.html('');
     var current = output.height();
-    if(!handler) handler = function() {};
-    if(originalHeight === current) return handler();
+    if(!cbk) cbk = function() {};
+    if(originalHeight === current) return cbk();
 
     output.height(originalHeight);
-    output.once('transitionend', handler);
+    output.once('transitionend', cbk);
   };
 
   window.println = function(text) {
@@ -55,12 +57,13 @@
   };
 
   window.frontendClass = function(handler) {
-    handlers.push(handler);
+    stack.push(handler);
   };
 
-  runButton.addEventListener('click', runHandlers);
-  window.addEventListener('keypress', function(e) {
-    if(e.which === 13) runHandlers();
+  runButton.on('click', run);
+  J(window).on('keypress', function(e) {
+    if(running) return;
+    if(e.which === 13) run();
     else if(e.which === 99) resetDisplay();
   });
 }(window);
